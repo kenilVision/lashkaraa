d65f1479e53f9a6e705c0684874e6f3433e48281
Kenil-M-patel <kenilp1911@gamil.com>
2025-05-18 12:04:39 +0530

    SDFS

--- server/src/api/v1/authorization/authorizationController.js
+++ server/src/api/v1/authorization/authorizationController.js
@@ -3,14 +3,25 @@ const bcrypt = require("bcryptjs");
 const SALT_ROUNDS = 10;
 const JWT_SECRET = process.env.JWT_SECRET;
 const User = require("../../../db/model/user");
+const ejs = require('ejs');
+const nodemailer = require('nodemailer');
+const path = require('path');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD
+  }
+});
 
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ success: false, message: "Email and password are required" });
-      }
+      return res.status(400).json({ success: false, message: "Email and password are required" });
+    }
 
     const user = await User.findOne({ email });
     if (!user) {
@@ -18,7 +29,7 @@ const login = async (req, res) => {
         .status(404)
         .json({ success: false, message: "Delivery agent not found!" });
     }
-    
+
     console.log(password, user.password)
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
@@ -136,14 +147,14 @@ const signup = async (req, res) => {
 
 const fetchUser = async (req, res) => {
   try {
-    const userId = req.user.userId; 
-    
+    const userId = req.user.userId;
+
     const user = await User.findById(userId).select('-password'); // Exclude password from response
-    
+
     if (!user) {
-      return res.status(404).json({ 
-        success: false, 
-        message: "User not found!" 
+      return res.status(404).json({
+        success: false,
+        message: "User not found!"
       });
     }
 
@@ -160,11 +171,71 @@ const fetchUser = async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ 
-      success: false, 
-      message: error.message 
+    return res.status(500).json({
+      success: false,
+      message: error.message
     });
   }
 };
 
-module.exports = { login, signup, update , fetchUser };
+// Forgot Password Apis
+const verifyEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(404).json({ success: false, message: "User not found!" });
+
+    const clientURI = `${process.env.CLIENT_URL}/account/reset/${user._id}`
+    const templatePath = path.join(__dirname, '../../../templates/reset-password.ejs');
+    const emailBody = await ejs.renderFile(templatePath, { clientURI, URI: process.env.CLIENT_URL });
+
+    const mailOptions = {
+      from: 'store+66037121243@t.shopifyemail.com',
+      to: user.email,
+      subject: 'Customer account password reset',
+      html: emailBody
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+    return res.status(200).json({ success: true, message: "Password reset link sent successfully." })
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { userId, password } = req.body;
+
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ success: false, message: "User not found!" });
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({ success: true, message: "Password reset successfully!" });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+}
+
+module.exports = { login, signup, update, fetchUser, verifyEmail, resetPassword };
--- server/src/api/v1/authorization/authorizationRoute.js
+++ server/src/api/v1/authorization/authorizationRoute.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const { login , signup , update , fetchUser } = require('./authorizationController');
+const { login, signup, update, fetchUser, verifyEmail, resetPassword } = require('./authorizationController');
 const auth = require('../../../middlewares/auth')
 
 
 router.post('/login', login);
-router.post('/signup', signup); 
+router.post('/signup', signup);
 router.put('/update', auth, update);
 router.get('/user', auth, fetchUser);
+router.post('/forgot-password', verifyEmail);
+router.post('/reset-password', resetPassword);
 
-module.exports = router;            
+module.exports = router;                
